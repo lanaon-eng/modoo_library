844cdf61f5643c5e56eaf08ff7a871e5fdd55e9e
lanaon-eng <lanaon@gmail.com>
2026-09-11 14:33:11 +0900

    Update logo image URL

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,1 +1,1 @@
-export const LOGO_URL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/asset/Gemini_Generated_Image_c6kcdfc6kcdfc6kc.png`;
+export const LOGO_URL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/asset/Code_Generated_Image.png`;
