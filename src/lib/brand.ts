c1619838780119f157640b4787dc04a4f829053d
lanaon-eng <lanaon@gmail.com>
2026-09-11 14:24:46 +0900

    Updated brand.ts

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,1 +1,1 @@
-export const LOGO_URL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/asset/Gemini_Generated_Image_v2x2suv2x2suv2x2%20(1).png`;
+export const LOGO_URL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/asset/Gemini_Generated_Image_c6kcdfc6kcdfc6kc.png`;
